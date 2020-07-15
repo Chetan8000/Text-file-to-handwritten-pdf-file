34014bff86cb58115db07b6eb2d16c1413d357db
Chetan <chetan.borse@47billion.com>
2020-07-15 22:53:31 +0530

    format_py_code

--- ScannedImages-to-PDF.py
+++ ScannedImages-to-PDF.py
@@ -9,22 +9,21 @@ import os
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i",
-                "--image",
-                required=True,
-                help="path to input image to be OCR'd")
-ap.add_argument("-p",
-                "--preprocess",
-                type=str,
-                default="thresh",
-                help="type of preprocessing to be done")
+ap.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
+ap.add_argument(
+    "-p",
+    "--preprocess",
+    type=str,
+    default="thresh",
+    help="type of preprocessing to be done",
+)
 args = vars(ap.parse_args())
 
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#cv2.imshow("Image", gray)
+# cv2.imshow("Image", gray)
 
 # check to see if we should apply thresholding to preprocess the
 # image
@@ -43,7 +42,7 @@ cv2.imwrite(filename, gray)
 
 # load the image as a PIL/Pillow image, apply OCR, and then delete
 # the temporary file
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 text = pytesseract.image_to_string(Image.open(filename))
 os.remove(filename)
 print(text.encode("utf-8"))
@@ -55,18 +54,18 @@ file.close()
 
 # show the output images
 # cv2.imshow("Image", image)
-#cv2.imshow("Output", gray)
+# cv2.imshow("Output", gray)
 cv2.waitKey(0)
 print("------------------- processed OCR -------------------")
 BG = Image.open("myfont\\bg.png")
 sizeOfSheet = BG.width
 gap, _ = 0, 0
-allowedChars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.-?!() 1234567890'
+allowedChars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.-?!() 1234567890"
 
 
 def writee(char):
     global gap, _
-    if char == '\n':
+    if char == "\n":
         pass
     else:
         char.lower()
@@ -88,44 +87,44 @@ def letterwrite(word):
                 pass
             elif letter.isupper():
                 letter = letter.lower()
-                letter += 'upper'
-            elif letter == '.':
+                letter += "upper"
+            elif letter == ".":
                 letter = "fullstop"
-            elif letter == '!':
-                letter = 'exclamation'
-            elif letter == '?':
-                letter = 'question'
-            elif letter == ',':
-                letter = 'comma'
-            elif letter == '(':
-                letter = 'braketop'
-            elif letter == ')':
-                letter = 'braketclose'
-            elif letter == '-':
-                letter = 'hiphen'
+            elif letter == "!":
+                letter = "exclamation"
+            elif letter == "?":
+                letter = "question"
+            elif letter == ",":
+                letter = "comma"
+            elif letter == "(":
+                letter = "braketop"
+            elif letter == ")":
+                letter = "braketclose"
+            elif letter == "-":
+                letter = "hiphen"
             writee(letter)
 
 
 def worddd(Input):
-    wordlist = Input.split(' ')
+    wordlist = Input.split(" ")
     for i in wordlist:
         letterwrite(i)
-        writee('space')
+        writee("space")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        with open('testfile.txt', 'r') as file:
-            data = file.read().replace('\n', '')
+        with open("testfile.txt", "r") as file:
+            data = file.read().replace("\n", "")
         l = len(data)
         nn = len(data) // 600
         chunks, chunk_size = len(data), len(data) // (nn + 1)
-        p = [data[i:i + chunk_size] for i in range(0, chunks, chunk_size)]
+        p = [data[i : i + chunk_size] for i in range(0, chunks, chunk_size)]
 
         for i in range(0, len(p)):
             worddd(p[i])
-            writee('\n')
-            BG.save('myfont/%doutt.png' % i)
+            writee("\n")
+            BG.save("myfont/%doutt.png" % i)
             BG1 = Image.open("myfont/bg.png")
             BG = BG1
             gap = 0
@@ -133,13 +132,14 @@ if __name__ == '__main__':
     except ValueError as E:
         print("{}\nTry again".format(E))
 
-#for conversion of img to pdf
+# for conversion of img to pdf
 from fpdf import FPDF
 from PIL import Image
+
 print("------------------- Converting to PDF -------------------")
 imagelist = []
 for i in range(0, len(p)):
-    imagelist.append('myfont/%doutt.png' % i)
+    imagelist.append("myfont/%doutt.png" % i)
 cover = Image.open(imagelist[0])
 width, height = cover.size
 pdf = FPDF(unit="pt", format=[width, height])
--- txttohandwriting.py
+++ txttohandwriting.py
@@ -2,15 +2,16 @@ from PIL import Image
 from fpdf import FPDF
 from PIL import Image
 import os
+
 BG = Image.open("myfont/bg.png")
 sizeOfSheet = BG.width
 gap, _ = 0, 0
-allowedChars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.-?!() 1234567890'
+allowedChars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.-?!() 1234567890"
 
 
 def writee(char):
     global gap, _
-    if char == '\n':
+    if char == "\n":
         pass
     else:
         char.lower()
@@ -32,48 +33,48 @@ def letterwrite(word):
                 pass
             elif letter.isupper():
                 letter = letter.lower()
-                letter += 'upper'
-            elif letter == '.':
+                letter += "upper"
+            elif letter == ".":
                 letter = "fullstop"
-            elif letter == '!':
-                letter = 'exclamation'
-            elif letter == '?':
-                letter = 'question'
-            elif letter == ',':
-                letter = 'comma'
-            elif letter == '(':
-                letter = 'braketop'
-            elif letter == ')':
-                letter = 'braketcl'
-            elif letter == '-':
-                letter = 'hiphen'
+            elif letter == "!":
+                letter = "exclamation"
+            elif letter == "?":
+                letter = "question"
+            elif letter == ",":
+                letter = "comma"
+            elif letter == "(":
+                letter = "braketop"
+            elif letter == ")":
+                letter = "braketcl"
+            elif letter == "-":
+                letter = "hiphen"
             writee(letter)
 
 
 def worddd(Input):
-    wordlist = Input.split(' ')
+    wordlist = Input.split(" ")
     for i in wordlist:
         letterwrite(i)
-        writee('space')
+        writee("space")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        with open('boom.txt', 'r') as file:
-            data = file.read().replace('\n', '')
+        with open("boom.txt", "r") as file:
+            data = file.read().replace("\n", "")
 
-        with open('final_output.pdf', 'w') as file:
+        with open("final_output.pdf", "w") as file:
             pass
 
         l = len(data)
         nn = len(data) // 600
         chunks, chunk_size = len(data), len(data) // (nn + 1)
-        p = [data[i:i + chunk_size] for i in range(0, chunks, chunk_size)]
+        p = [data[i : i + chunk_size] for i in range(0, chunks, chunk_size)]
 
         for i in range(0, len(p)):
             worddd(p[i])
-            writee('\n')
-            BG.save('%doutt.png' % i)
+            writee("\n")
+            BG.save("%doutt.png" % i)
             BG1 = Image.open("myfont/bg.png")
             BG = BG1
             gap = 0
@@ -83,24 +84,25 @@ if __name__ == '__main__':
 
 imagelist = []
 for i in range(0, len(p)):
-    imagelist.append('%doutt.png' % i)
+    imagelist.append("%doutt.png" % i)
 
-#Converting images to pdf
-#Source:https://datatofish.com/images-to-pdf-python/
+# Converting images to pdf
+# Source:https://datatofish.com/images-to-pdf-python/
 
 
 def pdf_creation(PNG_FILE, flag=False):
     rgba = Image.open(PNG_FILE)
-    rgb = Image.new('RGB', rgba.size, (255, 255, 255))  # white background
+    rgb = Image.new("RGB", rgba.size, (255, 255, 255))  # white background
     rgb.paste(rgba, mask=rgba.split()[3])  # paste using alpha channel as mask
-    rgb.save('final_output.pdf',
-             append=flag)  #Now save multiple images in same pdf file
+    rgb.save(
+        "final_output.pdf", append=flag
+    )  # Now save multiple images in same pdf file
 
 
-#First create a pdf file if not created
+# First create a pdf file if not created
 pdf_creation(imagelist.pop(0))
 
-#Now I am opening each images and converting them to pdf
-#Appending them to pdfs
+# Now I am opening each images and converting them to pdf
+# Appending them to pdfs
 for PNG_FILE in imagelist:
     pdf_creation(PNG_FILE, flag=True)
